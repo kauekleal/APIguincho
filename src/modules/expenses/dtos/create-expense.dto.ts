1a76a7a28e2ba6c09b659c09ee8e27788de96d9c
Kaue Leal <kauekleal@gmail.com>
2026-04-29 13:32:14 -0300

    fix: increase description field length to 100

--- src/modules/expenses/dtos/create-expense.dto.ts
+++ src/modules/expenses/dtos/create-expense.dto.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const createExpenseSchema = z.object({
   category: z.string().min(1),
   value: z.number().positive(),
-  description: z.string().max(11),
+  description: z.string().max(100),
 });
 
 export type CreateExpenseDto = z.infer<typeof createExpenseSchema>;
--- src/modules/expenses/dtos/update-expense.dto.ts
+++ src/modules/expenses/dtos/update-expense.dto.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const updateExpenseSchema = z.object({
   category: z.string().min(1).optional(),
   value: z.number().positive().optional(),
-  description: z.string().max(11).optional(),
+  description: z.string().max(100).optional(),
 });
 
 export type UpdateExpenseDto = z.infer<typeof updateExpenseSchema>;
